3800c3c4bea1b6695d20ab42c368ccec5d9b284b
Fahad Ali <fahadshykh369@gmail.com>
2022-04-22 01:44:05 +0500

    init

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    "mongodb+srv://fahadali:HWMeAZwq938KdpzT@cluster0.62413.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  SECRET_KEY: "some very secret key",
+};
